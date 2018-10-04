bb89aa1e2880fa8b795e32f32df491de67a32403
saridpong <saridpong@gmail.com>
2018-10-04 18:33:22 +0700

    update global

--- PrandaVehicleWeb/src/app/globals/globals.ts
+++ PrandaVehicleWeb/src/app/globals/globals.ts
@@ -3,8 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Globals {
     authen: AuthenData;
-    public BaseApiUrl: string = 'https://prandadev.azurewebsites.net/';
-    // public BaseApiUrl: string = 'http://10.10.0.29/carapi/';
+     public BaseApiUrl: string = 'http://10.10.0.29/carapi/';
 
     public setCookie(key: string, value: any): any {
         window.localStorage.setItem(key, JSON.stringify(value))
